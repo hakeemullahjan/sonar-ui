782b04c434dd29e7ad4241544784789f3dc1d91f
hakeemullahjan <hakeemullah.yousufzai@gmail.com>
2023-04-10 23:36:04 +0500

    add: css

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -3,6 +3,8 @@ import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
 
+import postcss from "rollup-plugin-postcss";
+
 // const packageJson = require("./package.json");
 import packageJson from "./package.json" assert { type: "json" };
 
@@ -25,11 +27,13 @@ export default [
       resolve(),
       commonjs(),
       typescript({ tsconfig: "./tsconfig.json" }),
+      postcss(),
     ],
   },
   {
     input: "dist/esm/types/index.d.ts",
     output: [{ file: "dist/index.d.ts", format: "esm" }],
     plugins: [dts()],
+    external: [/\.css$/],
   },
 ];
